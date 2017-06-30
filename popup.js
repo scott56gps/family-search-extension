461cde9ca890a76b09974cd57ec72b8530d7ca39
scott56gps <scott93gps@gmail.com>
2017-06-29 18:44:51 -0600

    Initial commit

---
+++ popup.js
@@ -0,0 +1,15 @@
+/********************************************************
+ * Popup.js
+ * Author: Scott Nicholes
+ * 
+ * This is a program that will scrape a Family Search
+ * Person Page for their name and ID.
+ *******************************************************/
+
+function scrapeNameAndId() {
+
+}
+
+function renderNameAndId(name, id) {
+
+}
