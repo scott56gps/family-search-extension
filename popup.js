06e11cf3102e4da543127b9c7ad4e79ac79176b5
scott56gps <scott93gps@gmail.com>
2017-07-18 18:45:40 -0600

    Attempting auth functionality on a background page.

--- popup.js
+++ popup.js
@@ -1,25 +1,35 @@
 /********************************************************
  * Popup.js
  * Author: Scott Nicholes
- * 
+ *
  * This is a program that will scrape a Family Search
  * Person Page for their name and ID.
  *******************************************************/
-
 function main() {
     // Scrape the page
     chrome.tabs.executeScript(null, {
         file: "./scraperCode.js"
         //code: 'console.log(document.querySelector(\'span.fs-person-vitals__name-full\').innerText)'
     }, function (results) {
+      if(Array.isArray(results)){
         console.log(results[0]);
-        document.getElementById('name-result').innerText = 'Name: ' + results[0].fullName;
-        document.getElementById('id-result').innerText = 'ID: ' + results[0].id;
+        // Send the results to our background page for processing
+        var backgroundPage = chrome.extension.getBackgroundPage();
+        
+        // document.getElementById('name-result').innerText = 'Name: ' + results[0].fullName;
+        // document.getElementById('id-result').innerText = 'ID: ' + results[0].id;
+      } else {
+        console.log('result is not an array');
+      }
     });
+
+	// Now, let's authenticate the user
 }
 
-function renderNameAndId(name, id) {
 
-}
 
-main();
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('hi');
+})
+
+document.addEventListener('DOMContentLoaded', main);
